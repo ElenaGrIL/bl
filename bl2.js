64755a7fb4dd6f271668e88e5c9efaec2fb5a3e4
ElenaGrIL <grishina.sk@mail.ru>
2026-09-05 14:14:20 +0300

    Update bl2.js

--- bl2.js
+++ bl2.js
@@ -1,11 +1,12 @@
 (function () {
 'use strict';
 
-if (window.plugin_bl2_ready) return;
-window.plugin_bl2_ready = true;
+if (window.plugin_bl2_bldub_v1) return;
+window.plugin_bl2_bldub_v1 = true;
 
-var LAKORNLAND =
-    'https://me14.lakornland.land/all';
+var API =
+    'https://bldub.com/api/v3/index.php?' +
+    'a=GetTitles&limit=8&sort=0';
 
 var ICON =
     '<svg viewBox="0 0 24 24" fill="currentColor">' +
@@ -18,19 +19,274 @@ var ICON =
     'L12 21.35z"/></svg>';
 
 
-function openLakornland() {
+function fixItem(item) {
 
-    Lampa.Noty.show(
-        'Открываю Lakornland...'
-    );
+    var d = item.data || {};
 
-    setTimeout(
+    return {
+        id: item.id,
+
+        title:
+            d.name || d.aka || 'Без названия',
+
+        name:
+            d.name || d.aka || 'Без названия',
+
+        original_title:
+            d.aka || '',
+
+        year:
+            d.year || '',
+
+        release_year:
+            d.year || '',
+
+        overview:
+            d.description || '',
+
+        description:
+            d.description || '',
+
+        genres:
+            d.genre || '',
+
+        country:
+            d.country || '',
+
+        status:
+            d.status || '',
+
+        type:
+            d.type || '',
+
+        episodes:
+            d.episodes || item.episodes || '',
+
+        translations:
+            item.translations || [],
+
+        vote_average:
+            item.rating &&
+            item.rating.avg
+                ? item.rating.avg
+                : 0,
+
+        img:
+            d.image || '',
+
+        poster:
+            d.image || '',
+
+        background_image:
+            d.image || '',
+
+        bldub_id:
+            item.id,
+
+        bldub_url:
+            'https://bldub.com/title/' +
+            item.id,
+
+        source:
+            'bldub'
+    };
+}
+
+
+function showInfo(item) {
+
+    var lines = [];
+
+    if (item.year)
+        lines.push(
+            'Год: ' + item.year
+        );
+
+    if (item.country)
+        lines.push(
+            'Страна: ' + item.country
+        );
+
+    if (item.genres)
+        lines.push(
+            'Жанр: ' + item.genres
+        );
+
+    if (item.status)
+        lines.push(
+            'Статус: ' + item.status
+        );
+
+    if (item.episodes)
+        lines.push(
+            'Серий: ' + item.episodes
+        );
+
+    if (
+        item.translations &&
+        item.translations.length
+    ) {
+        lines.push(
+            'Перевод: ' +
+            item.translations.join(', ')
+        );
+    }
+
+    if (item.vote_average) {
+        lines.push(
+            'Рейтинг: ' +
+            Number(
+                item.vote_average
+            ).toFixed(1)
+        );
+    }
+
+    if (item.original_title) {
+        lines.push(
+            'Оригинальное название: ' +
+            item.original_title
+        );
+    }
+
+    if (item.description) {
+        lines.push('');
+        lines.push(
+            item.description
+        );
+    }
+
+    Lampa.Modal.open({
+        title:
+            item.title,
+
+        html:
+            $('<div style="' +
+              'font-size:1.1em;' +
+              'line-height:1.5;' +
+              'padding:1em;">' +
+              lines
+                .join('<br>')
+              +
+              '</div>'),
+
+        size:
+            'medium',
+
+        onBack:
+            function () {
+                Lampa.Modal.close();
+                Lampa.Controller.toggle(
+                    'content'
+                );
+            }
+    });
+}
+
+
+function BL2(object) {
+
+    var comp =
+        new Lampa.InteractionMain(
+            object
+        );
+
+    var network =
+        new Lampa.Reguest();
+
+
+    comp.create =
         function () {
-            window.location.href =
-                LAKORNLAND;
-        },
-        300
-    );
+
+            var _this = this;
+
+            this.activity
+                .loader(true);
+
+            network.silent(
+                API,
+
+                function (json) {
+
+                    var result = [];
+
+                    if (
+                        json &&
+                        json.result
+                    ) {
+                        result =
+                            json.result.map(
+                                fixItem
+                            );
+                    }
+
+                    if (!result.length) {
+
+                        _this.empty();
+                        return;
+                    }
+
+                    Lampa.Utils
+                        .extendItemsParams(
+                            result,
+                            {
+                                style: {
+                                    name:
+                                        'wide'
+                                }
+                            }
+                        );
+
+                    _this.build([
+                        {
+                            title:
+                                '❤️ BLDUB',
+
+                            results:
+                                result
+                        }
+                    ]);
+
+                    _this.activity
+                        .loader(false);
+                },
+
+                function () {
+
+                    _this.activity
+                        .loader(false);
+
+                    _this.empty();
+                }
+            );
+
+            return this.render();
+        };
+
+
+    comp.onAppend =
+        function (
+            line,
+            element
+        ) {
+
+            line.onAppend =
+                function (card) {
+
+                    card.onEnter =
+                        function (
+                            target,
+                            card_data
+                        ) {
+
+                            showInfo(
+                                card_data
+                            );
+                        };
+                };
+        };
+
+
+    return comp;
 }
 
 
@@ -53,7 +309,7 @@ function addButton() {
 
     if (
         menu.find(
-            '.menu__item[data-sid="bl2"]'
+            '.menu__item[data-sid="bl2-bldub"]'
         ).length
     ) return;
 
@@ -62,8 +318,8 @@ function addButton() {
 
         '<li ' +
         'class="menu__item selector" ' +
-        'data-action="bl2_action" ' +
-        'data-sid="bl2">' +
+        'data-action="bl2_bldub" ' +
+        'data-sid="bl2-bldub">' +
 
         '<div class="menu__ico">' +
         ICON +
@@ -81,7 +337,14 @@ function addButton() {
         'hover:enter',
 
         function () {
-            openLakornland();
+
+            Lampa.Activity.push({
+                title:
+                    'BL2 · BLDUB',
+
+                component:
+                    'bl2_bldub_main'
+            });
         }
     );
 
@@ -90,9 +353,34 @@ function addButton() {
 }
 
 
-if (window.appready) {
+function startPlugin() {
+
+    Lampa.Component.add(
+        'bl2_bldub_main',
+        BL2
+    );
 
     addButton();
+
+
+    setInterval(
+        function () {
+
+            if (
+                window.appready
+            ) {
+                addButton();
+            }
+        },
+
+        4000
+    );
+}
+
+
+if (window.appready) {
+
+    startPlugin();
 
 } else {
 
@@ -102,24 +390,13 @@ if (window.appready) {
         function (e) {
 
             if (
-                e.type === 'ready'
+                e.type ===
+                'ready'
             ) {
-                addButton();
+                startPlugin();
             }
         }
     );
 }
-
-
-setInterval(
-    function () {
-
-        if (window.appready) {
-            addButton();
-        }
-    },
-
-    4000
-);
 
 })();
