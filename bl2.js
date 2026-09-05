2fe2aff30c34e1fca208cba684a99ba11a46346c
ElenaGrIL <grishina.sk@mail.ru>
2026-09-05 14:26:11 +0300

    Update bl2.js

--- bl2.js
+++ bl2.js
@@ -1,12 +1,14 @@
 (function () {
 'use strict';
 
-if (window.plugin_bl2_bldub_v1) return;
-window.plugin_bl2_bldub_v1 = true;
+if (window.plugin_bl2_bldub_v2) return;
+window.plugin_bl2_bldub_v2 = true;
+
 
 var API =
     'https://bldub.com/api/v3/index.php?' +
     'a=GetTitles&limit=8&sort=0';
+
 
 var ICON =
     '<svg viewBox="0 0 24 24" fill="currentColor">' +
@@ -19,18 +21,40 @@ var ICON =
     'L12 21.35z"/></svg>';
 
 
-function fixItem(item) {
+function convertItem(item) {
 
     var d = item.data || {};
 
+    var rating = 0;
+
+    if (
+        item.rating &&
+        item.rating.avg
+    ) {
+        rating =
+            Number(
+                item.rating.avg
+            );
+    }
+
+
     return {
-        id: item.id,
+
+        id:
+            item.id,
+
+        bldub_id:
+            item.id,
 
         title:
-            d.name || d.aka || 'Без названия',
+            d.name ||
+            d.aka ||
+            'Без названия',
 
         name:
-            d.name || d.aka || 'Без названия',
+            d.name ||
+            d.aka ||
+            'Без названия',
 
         original_title:
             d.aka || '',
@@ -38,20 +62,17 @@ function fixItem(item) {
         year:
             d.year || '',
 
-        release_year:
-            d.year || '',
+        country:
+            d.country || '',
 
-        overview:
-            d.description || '',
+        genre:
+            d.genre || '',
 
         description:
             d.description || '',
 
-        genres:
-            d.genre || '',
-
-        country:
-            d.country || '',
+        overview:
+            d.description || '',
 
         status:
             d.status || '',
@@ -60,16 +81,18 @@ function fixItem(item) {
             d.type || '',
 
         episodes:
-            d.episodes || item.episodes || '',
+            d.episodes ||
+            item.episodes ||
+            '',
+
+        premium:
+            d.premium || '',
 
         translations:
             item.translations || [],
 
         vote_average:
-            item.rating &&
-            item.rating.avg
-                ? item.rating.avg
-                : 0,
+            rating,
 
         img:
             d.image || '',
@@ -79,9 +102,6 @@ function fixItem(item) {
 
         background_image:
             d.image || '',
-
-        bldub_id:
-            item.id,
 
         bldub_url:
             'https://bldub.com/title/' +
@@ -93,88 +113,222 @@ function fixItem(item) {
 }
 
 
-function showInfo(item) {
+function escapeHtml(text) {
 
-    var lines = [];
+    return String(text || '')
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
 
-    if (item.year)
-        lines.push(
-            'Год: ' + item.year
-        );
 
-    if (item.country)
-        lines.push(
-            'Страна: ' + item.country
-        );
+function showDetails(item) {
 
-    if (item.genres)
-        lines.push(
-            'Жанр: ' + item.genres
-        );
-
-    if (item.status)
-        lines.push(
-            'Статус: ' + item.status
-        );
-
-    if (item.episodes)
-        lines.push(
-            'Серий: ' + item.episodes
-        );
+    var translation = '';
 
     if (
         item.translations &&
         item.translations.length
     ) {
-        lines.push(
-            'Перевод: ' +
-            item.translations.join(', ')
-        );
+        translation =
+            item.translations.join(', ');
     }
+
+
+    var rating = '';
 
     if (item.vote_average) {
-        lines.push(
-            'Рейтинг: ' +
+
+        rating =
             Number(
                 item.vote_average
-            ).toFixed(1)
-        );
+            ).toFixed(1);
     }
+
+
+    var html = '';
+
+    html +=
+        '<div style="' +
+        'padding:1.2em;' +
+        'line-height:1.5;' +
+        'font-size:1.05em;">';
+
+
+    if (item.img) {
+
+        html +=
+            '<div style="' +
+            'display:flex;' +
+            'gap:1.2em;' +
+            'align-items:flex-start;">';
+
+
+        html +=
+            '<img src="' +
+            escapeHtml(item.img) +
+            '" style="' +
+            'width:11em;' +
+            'max-height:16em;' +
+            'object-fit:cover;' +
+            'border-radius:.5em;">';
+
+
+        html +=
+            '<div>';
+    }
+
 
     if (item.original_title) {
-        lines.push(
-            'Оригинальное название: ' +
-            item.original_title
-        );
+
+        html +=
+            '<div><b>Оригинальное:</b> ' +
+            escapeHtml(
+                item.original_title
+            ) +
+            '</div>';
     }
+
+
+    if (item.year) {
+
+        html +=
+            '<div><b>Год:</b> ' +
+            escapeHtml(
+                item.year
+            ) +
+            '</div>';
+    }
+
+
+    if (item.country) {
+
+        html +=
+            '<div><b>Страна:</b> ' +
+            escapeHtml(
+                item.country
+            ) +
+            '</div>';
+    }
+
+
+    if (item.type) {
+
+        html +=
+            '<div><b>Тип:</b> ' +
+            escapeHtml(
+                item.type
+            ) +
+            '</div>';
+    }
+
+
+    if (item.genre) {
+
+        html +=
+            '<div><b>Жанр:</b> ' +
+            escapeHtml(
+                item.genre
+            ) +
+            '</div>';
+    }
+
+
+    if (item.status) {
+
+        html +=
+            '<div><b>Статус:</b> ' +
+            escapeHtml(
+                item.status
+            ) +
+            '</div>';
+    }
+
+
+    if (item.episodes) {
+
+        html +=
+            '<div><b>Серий:</b> ' +
+            escapeHtml(
+                item.episodes
+            ) +
+            '</div>';
+    }
+
+
+    if (translation) {
+
+        html +=
+            '<div><b>Перевод:</b> ' +
+            escapeHtml(
+                translation
+            ) +
+            '</div>';
+    }
+
+
+    if (rating) {
+
+        html +=
+            '<div><b>Рейтинг:</b> ' +
+            escapeHtml(
+                rating
+            ) +
+            '</div>';
+    }
+
+
+    html +=
+        '<div><b>BLDUB ID:</b> ' +
+        escapeHtml(
+            item.bldub_id
+        ) +
+        '</div>';
+
+
+    if (item.img) {
+
+        html +=
+            '</div></div>';
+    }
+
 
     if (item.description) {
-        lines.push('');
-        lines.push(
-            item.description
-        );
+
+        html +=
+            '<div style="' +
+            'margin-top:1.2em;' +
+            'font-size:.95em;">' +
+
+            escapeHtml(
+                item.description
+            ) +
+
+            '</div>';
     }
 
+
+    html += '</div>';
+
+
     Lampa.Modal.open({
+
         title:
             item.title,
 
         html:
-            $('<div style="' +
-              'font-size:1.1em;' +
-              'line-height:1.5;' +
-              'padding:1em;">' +
-              lines
-                .join('<br>')
-              +
-              '</div>'),
+            $(html),
 
         size:
             'medium',
 
         onBack:
             function () {
+
                 Lampa.Modal.close();
+
                 Lampa.Controller.toggle(
                     'content'
                 );
@@ -186,104 +340,142 @@ function showInfo(item) {
 function BL2(object) {
 
     var comp =
-        new Lampa.InteractionMain(
+        Lampa.Maker.make(
+            'Main',
             object
         );
 
-    var network =
-        new Lampa.Reguest();
+
+    comp.use({
+
+        onCreate:
+            function () {
+
+                var self =
+                    this;
+
+                self.activity
+                    .loader(true);
 
 
-    comp.create =
-        function () {
-
-            var _this = this;
-
-            this.activity
-                .loader(true);
-
-            network.silent(
-                API,
-
-                function (json) {
-
-                    var result = [];
-
-                    if (
-                        json &&
-                        json.result
-                    ) {
-                        result =
-                            json.result.map(
-                                fixItem
-                            );
-                    }
-
-                    if (!result.length) {
-
-                        _this.empty();
-                        return;
-                    }
-
-                    Lampa.Utils
-                        .extendItemsParams(
-                            result,
-                            {
-                                style: {
-                                    name:
-                                        'wide'
-                                }
-                            }
-                        );
-
-                    _this.build([
-                        {
-                            title:
-                                '❤️ BLDUB',
-
-                            results:
-                                result
-                        }
-                    ]);
-
-                    _this.activity
-                        .loader(false);
-                },
-
-                function () {
-
-                    _this.activity
-                        .loader(false);
-
-                    _this.empty();
-                }
-            );
-
-            return this.render();
-        };
+                var network =
+                    new Lampa.Reguest();
 
 
-    comp.onAppend =
-        function (
-            line,
-            element
-        ) {
+                network.silent(
 
-            line.onAppend =
-                function (card) {
+                    API,
 
-                    card.onEnter =
-                        function (
-                            target,
-                            card_data
+                    function (json) {
+
+                        self.activity
+                            .loader(false);
+
+
+                        var results = [];
+
+
+                        if (
+                            json &&
+                            json.result
                         ) {
 
-                            showInfo(
-                                card_data
-                            );
-                        };
-                };
-        };
+                            results =
+                                json.result.map(
+                                    convertItem
+                                );
+                        }
+
+
+                        if (!results.length) {
+
+                            self.empty();
+
+                            return;
+                        }
+
+
+                        self.build([
+                            {
+                                title:
+                                    '❤️ BLDUB',
+
+                                results:
+                                    results,
+
+                                params: {
+
+                                    items: {
+                                        view: 7
+                                    },
+
+                                    style: {
+                                        name:
+                                            'wide'
+                                    }
+                                }
+                            }
+                        ]);
+                    },
+
+
+                    function () {
+
+                        self.activity
+                            .loader(false);
+
+                        self.empty();
+                    }
+                );
+            },
+
+
+        onInstance:
+            function (
+                line,
+                lineData
+            ) {
+
+                line.use({
+
+                    onInstance:
+                        function (
+                            card,
+                            cardData
+                        ) {
+
+                            card.use({
+
+                                onlyEnter:
+                                    function () {
+
+                                        showDetails(
+                                            cardData
+                                        );
+                                    },
+
+
+                                onFocus:
+                                    function () {
+
+                                        if (
+                                            cardData
+                                                .background_image &&
+                                            Lampa.Background
+                                        ) {
+
+                                            Lampa.Background
+                                                .change(
+                                                    cardData
+                                                        .background_image
+                                                );
+                                        }
+                                    }
+                            });
+                        }
+                });
+            }
+    });
 
 
     return comp;
@@ -295,6 +487,7 @@ function addButton() {
     var menu =
         $('.menu .menu__list')
             .eq(0);
+
 
     if (!menu.length) {
 
@@ -309,17 +502,19 @@ function addButton() {
 
     if (
         menu.find(
-            '.menu__item[data-sid="bl2-bldub"]'
+            '[data-sid="bl2-bldub-v2"]'
         ).length
-    ) return;
+    ) {
+        return;
+    }
 
 
     var button = $(
 
         '<li ' +
         'class="menu__item selector" ' +
-        'data-action="bl2_bldub" ' +
-        'data-sid="bl2-bldub">' +
+        'data-action="bl2_bldub_v2" ' +
+        'data-sid="bl2-bldub-v2">' +
 
         '<div class="menu__ico">' +
         ICON +
@@ -334,36 +529,42 @@ function addButton() {
 
 
     button.on(
+
         'hover:enter',
 
         function () {
 
             Lampa.Activity.push({
+
                 title:
                     'BL2 · BLDUB',
 
                 component:
-                    'bl2_bldub_main'
+                    'bl2_bldub_v2'
             });
         }
     );
 
 
-    menu.append(button);
+    menu.append(
+        button
+    );
 }
 
 
 function startPlugin() {
 
     Lampa.Component.add(
-        'bl2_bldub_main',
+        'bl2_bldub_v2',
         BL2
     );
+
 
     addButton();
 
 
     setInterval(
+
         function () {
 
             if (
@@ -385,6 +586,7 @@ if (window.appready) {
 } else {
 
     Lampa.Listener.follow(
+
         'app',
 
         function (e) {
